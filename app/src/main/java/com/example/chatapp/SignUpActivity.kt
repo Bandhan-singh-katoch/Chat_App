78b62419650f9e91c9952e6414fe2b52a31e8826
Bandhan singh <parbhatbandhan@gmail.com>
2022-01-18 20:01:24 +0530

    user registration done

--- app/src/main/java/com/example/chatapp/SignUpActivity.kt
+++ app/src/main/java/com/example/chatapp/SignUpActivity.kt
@@ -1,10 +1,16 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -16,14 +22,53 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         auth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener{
+            val userName = etName.text.toString()
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+            val confirmPassword = etConfigurePassword.text.toString()
+
+            if(TextUtils.isEmpty(userName)){
+                Toast.makeText(applicationContext,"username is required",Toast.LENGTH_SHORT).show()
+            }
+            if(TextUtils.isEmpty(email)){
+                Toast.makeText(applicationContext,"email is required",Toast.LENGTH_SHORT).show()
+            }
+            if(TextUtils.isEmpty(password)){
+                Toast.makeText(applicationContext,"password is required",Toast.LENGTH_SHORT).show()
+            }
+            if(TextUtils.isEmpty(confirmPassword)){
+                Toast.makeText(applicationContext,"confirm password is required",Toast.LENGTH_SHORT).show()
+            }
+            if(confirmPassword != password){
+                Toast.makeText(applicationContext,"password is not matching",Toast.LENGTH_SHORT).show()
+            }
+            registerUser(userName,email,password)
+
+        }
     }
 
-    private fun registerUser(email:String,password:String){
+    private fun registerUser(userName:String,email:String,password:String){
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this){
                 if(it.isSuccessful){
-                    var user: FirebaseUser? = auth.currentUser
-                    var userId:String = user!!.uid
+                    val user: FirebaseUser? = auth.currentUser
+                    val userId:String = user!!.uid
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
+
+                    val hashMap:HashMap<String,String> = HashMap()
+                    hashMap.put("userId",userId)
+                    hashMap.put("userName",userName)
+                    hashMap.put("userImage","")
+
+                    databaseReference.setValue(hashMap).addOnCompleteListener(this){
+                        if(it.isSuccessful){
+                            val intent = Intent(this@SignUpActivity,HomeActivity::class.java)
+                            startActivity(intent)
+                        }
+                    }
                 }
             }
     }
