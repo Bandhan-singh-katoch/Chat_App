2aee5522f67eb56ca2dc038bf1042b9479fb4419
Bandhan singh <parbhatbandhan@gmail.com>
2022-01-27 00:22:33 +0530

    profile page is in progress

--- app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatapp.R
 
-class HomeActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_profile)
     }
 }
