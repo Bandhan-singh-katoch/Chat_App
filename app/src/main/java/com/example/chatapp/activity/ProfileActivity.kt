bf73dff0d362d878487ffc2510a01bb46c5c0ce0
Bandhan singh <parbhatbandhan@gmail.com>
2022-01-28 00:03:15 +0530

    image upload done and edit name done

--- app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
+++ app/src/main/java/com/example/chatapp/activity/ProfileActivity.kt
@@ -1,22 +1,31 @@
 package com.example.chatapp.activity
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.chatapp.R
 import com.example.chatapp.model.User
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.OnProgressListener
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_users.*
 import kotlinx.android.synthetic.main.activity_users.imgBack
 import java.io.IOException
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -24,8 +33,10 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var databaseReference: DatabaseReference
 
     private var filePath: Uri? = null
+    private val PICK_IMAGE_REQUEST: Int = 2020
 
-    private val PICK_IMAGE_REQUEST: Int = 2022
+    private lateinit var storage:FirebaseStorage
+    private lateinit var storageRef:StorageReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +45,13 @@ class ProfileActivity : AppCompatActivity() {
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
         databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.uid)
 
+        storage = FirebaseStorage.getInstance()
+        storageRef = storage.reference
+
         databaseReference.addValueEventListener(object :ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 val user = snapshot.getValue(User::class.java)
-                userName.text = user!!.userName
+                etUserName.setText(user!!.userName)
 
                 if(user.userImage == ""){
                     userImage.setImageResource(R.drawable.proflie_img)
@@ -56,6 +70,15 @@ class ProfileActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        userImage.setOnClickListener {
+            chooseImage()
+        }
+
+        btnSave.setOnClickListener {
+            uploadImage()
+            progressBar.visibility = View.VISIBLE
+        }
+
     }
 
     private fun chooseImage(){
@@ -70,11 +93,37 @@ class ProfileActivity : AppCompatActivity() {
         if(requestCode == PICK_IMAGE_REQUEST && resultCode != null){
             filePath = data!!.data
             try {
-                var bitmap:Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filePath)
+                val bitmap:Bitmap = MediaStore.Images.Media.getBitmap(contentResolver,filePath)
                 userImage.setImageBitmap(bitmap)
+                btnSave.visibility = View.VISIBLE
             }catch (e:IOException){
                 e.printStackTrace()
             }
+        }
+    }
+
+    private fun uploadImage(){
+        if(filePath != null){
+
+            val ref: StorageReference = storageRef.child("image/"+UUID.randomUUID().toString())
+            ref.putFile(filePath!!)
+                .addOnSuccessListener {
+
+                    val hashMap:HashMap<String,String> = HashMap()
+                    hashMap.put("userName",etUserName.text.toString())
+                    hashMap.put("userImage","")
+                    databaseReference.updateChildren(hashMap as Map<String,Any>)
+
+                    progressBar.visibility = View.GONE
+                    Toast.makeText(applicationContext,"Uploaded",Toast.LENGTH_SHORT).show()
+                    btnSave.visibility = View.GONE
+                }
+                .addOnFailureListener {
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(applicationContext,"Failed"+it.message,Toast.LENGTH_SHORT).show()
+
+                }
+
         }
     }
 }
